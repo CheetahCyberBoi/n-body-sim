64d2d5636cfe862f6911436ca2d7fec0c11bfe4e
Cheetah <DaverDiscord@gmail.com>
2025-12-31 20:51:55 -0800

    initial commit, static bodies

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use macroquad::prelude::*;
+
+struct Body {
+    position: Vec2,
+}
+
+struct State {
+    bodies: Vec<Body>,
+}
+
+impl State {
+    fn new(num_bodies: u32) -> Self {
+        let mut bodies = Vec::new();
+        for _ in 0..num_bodies - 1 {
+            bodies.push(Body {
+                position: Vec2::new(
+                    rand::gen_range(-screen_width(), screen_width()),
+                    rand::gen_range(-screen_height(), screen_height()),
+                ),
+            });
+        }
+        Self { bodies }
+    }
+    fn frame(&mut self) {
+        clear_background(BLACK);
+
+        for body in self.bodies.iter() {
+            draw_circle(body.position.x, body.position.y, 2.5, LIGHTGRAY);
+        }
+    }
+}
+
+#[macroquad::main("N-Body Simulation")]
+async fn main() {
+    let mut state = State::new(100);
+    loop {
+        state.frame();
+        next_frame().await
+    }
+}
